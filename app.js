f4e981750859794f0cf9f670a6b4b31a95fd1baf
pnchinmay <pnchinmay9@gmail.com>
2021-10-07 00:43:22 +0530

    Live excercise: QuerySelector

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 
-console.log(txtInput);
+// 1. textarea tag
+var textArea = document.querySelector("textarea")
+console.log(textArea)
+console.log("hello")
+
+// 2. btn-primary class
+var btnPrimary = document.querySelector(".btn-primary");
+console.log(btnPrimary)
+console.log("hello")
+
+// 3. input-btn id
+var inputBin = document.querySelector("#input-bin");
+console.log(inputBin)
+console.log("hello")
+
+// 4. input element with attribute name="translator"
+var translator = document.querySelector("input[name='translator']");
+console.log(translator)
+console.log("hello")
+
+
+
+
+// console.log(txtInput);
 
 
 function clickEventHandler() {
