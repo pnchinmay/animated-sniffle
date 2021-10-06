db9e406871d6e9cd87542b88eb53b2c7b95a1096
pnchinmay <pnchinmay9@gmail.com>
2021-10-06 23:33:11 +0530

    Taking input and console, textArea in HTML

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-var btnTranslate = document.querySelector("#btn-translate")
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+console.log(txtInput);
+
 
 function clickEventHandler() {
 console.log("clicked :)");
+console.log(txtInput.value)
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
