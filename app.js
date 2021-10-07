63156de8b9430bfea6e530e5d0f319145b5eb637
pnchinmay <pnchinmay9@gmail.com>
2021-10-07 17:45:47 +0530

    API call with displaying stringfied json in output field for the user as well as console

--- app.js
+++ app.js
@@ -2,20 +2,44 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output")
 
-var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am Ironman"
+const text = txtInput.value;
 
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=" + text
+
+// Method - 1
 // fetch(url)
 //     .then(response => response.json())
 //     .then(json => console.log(json))
-fetch(url)
-.then(function responseHandler(response) { return response.json() })
-.then(function logJSON(json) { console.log(json)})
+
+// Method - 2
+// fetch(url)
+// .then(function responseHandler(response) { return response.json() })
+// .then(function logJSON(json) { outputDiv.innerText = json; console.log(json)})
 
 // console.log(outputDiv);
 
 
 function clickEventHandler() {
-    outputDiv.innerText = "banana " + txtInput.value;
+    const text = txtInput.value;
+
+    var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=" + text
+
+    // outputDiv.innerText = "banana " + txtInput.value;
+
+    // Method - 1 -- easier way
+    fetch(url)
+        .then(response => response.json())
+        .then(json => outputDiv.innerText = JSON.stringify(json))
+
+    // Method - 2
+    // fetch(url)
+    //     .then(function responseHandler(response) {
+    //         return response.json()
+    //     })
+    //     .then(function logJSON(json) {
+    //         outputDiv.innerText = JSON.stringify(json);
+    //         console.log(json)
+    //     })
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
